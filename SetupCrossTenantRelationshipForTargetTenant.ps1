f45d15836a51848c6f58f712a1f118756c58b387
Roman Powell <ropow@microsoft.com>
2020-09-25 11:19:37 -0700

    More readability fixes.

--- SetupCrossTenantRelationshipForTargetTenant.ps1
+++ SetupCrossTenantRelationshipForTargetTenant.ps1
@@ -598,7 +598,7 @@ if (-not $enumExists) {
 function UserPrompt() {
     Write-Host `n
     Write-Host "Welcome to the Cross-tenant mailbox migration preview! Before running this script, please be sure to review the details provided on docs.microsoft.com at the following URL: `nhttps://docs.microsoft.com/en-us/microsoft-365/enterprise/cross-tenant-mailbox-migration"
-    Write-Host "It is also recommended before running this script to review the script in a script editor or Notepad prior to running."`n
+    Write-Host "`nIt is also recommended before running this script to review the script in a script editor or Notepad prior to running."`n
     Write-Host "For general feedback and / or questions, please contact crosstenantmigrationpreview@service.microsoft.com.`nThis is not a support alias and should not be used if you are currently experiencing an issue and need immediate assistance."`n
     $title = "Confirm: Configure Cross-Tenant mailbox migration preview."
     $message = "`nIf you are ready to begin configuring your tenants, select 'Y'.`nIf you need to review any additional details and proceed at a later time, select 'N'.`n`nDo you wish to proceed?"
